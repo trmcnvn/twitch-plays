0b6d3ac24100dce4e2ad34f3310543a1942fcf4e
Thomas McNiven <mail@vevix.net>
2014-02-18 05:52:25 +1000

    small recode using goroutine + channel

--- main.go
+++ main.go
@@ -1,109 +1,111 @@
 package main
 
 import (
+  "./lib/win32"
   "fmt"
-  "unsafe"
-  "time"
   "github.com/laurent22/toml-go"
   "github.com/thoj/go-ircevent"
-  "./lib"
+  "time"
 )
+
+type Twitch struct {
+  user    string
+  token   string
+  channel string
+}
+
+type Input struct {
+  user    string
+  message string
+  key     uint16
+}
 
 var (
   // emulator key: windows virtual key
   validKeys = map[string]uint16{
-    "a": win32.VK_X,
-    "b": win32.VK_Z,
-    "x": win32.VK_S,
-    "y": win32.VK_A,
-    "up": win32.VK_UP,
-    "left": win32.VK_LEFT,
-    "down": win32.VK_DOWN,
+    "a":     win32.VK_X,
+    "b":     win32.VK_Z,
+    "x":     win32.VK_S,
+    "y":     win32.VK_A,
+    "up":    win32.VK_UP,
+    "left":  win32.VK_LEFT,
+    "down":  win32.VK_DOWN,
     "right": win32.VK_RIGHT,
   }
 )
 
-// makes sure our config file includes the values we need
-func validateConf(doc toml.Document) {
-  _, user := doc.GetValue("twitch.user")
-  _, token := doc.GetValue("twitch.token")
-  _, channel := doc.GetValue("twitch.channel")
+func inputHandler(inputs <-chan Input) {
+  for {
+    input := <-inputs
+    fmt.Printf("Handling: %v - %v\n", input.user, input.message)
 
-  if !user || !token || !channel {
-    panic("Couldn't validate config file.")
-  }
-}
-
-// handles messages sent to the twitch channel
-func handleMessage(handle unsafe.Pointer, e *irc.Event) {
-  user, message := e.Nick, e.Message()
-  key, ok := validKeys[message]
-  if ok {
-    // send the input as a keydown event
     win32.SendInput(win32.INPUT{
       Type: win32.INPUT_KEYBOARD,
       Ki: win32.KEYBDINPUT{
-        WVk: key,
-        WScan: 0,
+        WVk:     input.key,
         DwFlags: win32.KEYEVENTF_KEYDOWN,
-        Time: 0,
-        DwExtraInfo: 0,
       },
     })
 
-    // give it some time
     time.Sleep(200 * time.Millisecond)
 
-    // resend the key as a keyup event
     win32.SendInput(win32.INPUT{
-      Type: win32.INPUT_KEYBOARD, 
+      Type: win32.INPUT_KEYBOARD,
       Ki: win32.KEYBDINPUT{
-        WVk: key,
-        WScan: 0,
+        WVk:     input.key,
         DwFlags: win32.KEYEVENTF_KEYUP,
-        Time: 0,
-        DwExtraInfo: 0,
       },
     })
-
-    // output on our console
-    fmt.Printf("%v: %v\n", user, message)
   }
 }
 
 func main() {
-  // parse and validate config
   var parser toml.Parser
   doc := parser.ParseFile("config/app.conf")
-  validateConf(doc)
+  twitch := Twitch{
+    user:    doc.GetString("twitch.user"),
+    token:   doc.GetString("twitch.token"),
+    channel: "#" + doc.GetString("twitch.channel"),
+  }
 
-  // find emulator window
+  inputs := make(chan Input)
+  go inputHandler(inputs)
+
+  // make sure emulator window is open
   handle := win32.FindWindow("DeSmuME", "DeSmuME 0.9.10 x64")
   if handle == nil {
     panic("Couldn't find emulator window.")
   }
   fmt.Printf("Emulator Window: %v\n", handle)
 
-  // connect to twitch IRC
-  user := doc.GetString("twitch.user")
-  conn := irc.IRC(user, user)
-  conn.Password = doc.GetString("twitch.token")
+  // connect to TwitchTV chat
+  conn := irc.IRC(twitch.user, twitch.user)
+  conn.Password = twitch.token
   err := conn.Connect("irc.twitch.tv:6667")
   if err != nil {
     panic("Couldn't connect to twitch.")
   }
 
-  // join channel on connectionc
-  channel := "#" + doc.GetString("twitch.channel")
-  conn.AddCallback("001", func (e *irc.Event) {
-    conn.Join(channel)
-    fmt.Printf("Joined the channel: %v\n", channel)
+  conn.AddCallback("001", func(e *irc.Event) {
+    conn.Join(twitch.channel)
+    fmt.Printf("Joined the channel: %v\n", twitch.channel)
   })
 
-  // handle messages to the channel
   conn.AddCallback("PRIVMSG", func(e *irc.Event) {
-    if e.Arguments[0] == channel {
-      handleMessage(handle, e)
+    if e.Arguments[0] != twitch.channel {
+      return
+    }
+
+    user, message := e.Nick, e.Message()
+    key, ok := validKeys[message]
+    if !ok {
+      return
+    }
+
+    inputs <- Input{
+      user:    user,
+      message: message,
+      key:     key,
     }
   })
 
